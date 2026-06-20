82a8a777987ea78b133b0bcfb92084c97ee30c51
jamieariailwatkins-web <jamie.ariail.watkins@gmail.com>
2026-06-19 20:09:45 -0400

    Update outFields in parcels API request

--- api/parcels.js
+++ api/parcels.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
   const params = new URLSearchParams({
     where,
-    outFields: 'AKPAR,WHOLE_ADDRESS,POSTAL,STATE,ZIP,ZONING,JURISDICT,JURISDICT2,DEED_ACRES,CURR_NAME1',
+    outFields: 'AKPAR,WHOLE_ADDRESS,POSTAL,STATE,ZIP,DIST_TWN,TAX_DISTRI,CURR_NAME1,DEEDAC,PID',
     returnGeometry: 'false',
     f: 'json',
     resultRecordCount: resultRecordCount || '10'
@@ -35,7 +35,4 @@ export default async function handler(req, res) {
     }
     const data = await upstream.json();
     return res.status(200).json(data);
-  } catch (e) {
-    return res.status(502).json({ error: e.message });
-  }
-}
+  } catch
